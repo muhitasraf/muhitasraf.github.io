00317a7145f3fd17bae4267ce8185ef92310ed89
muhitasraf <muhitasraf@gmail.com>
2022-11-27 16:57:38 +0600

    Contact page fully functional

--- mail/contact.js
+++ mail/contact.js
@@ -10,41 +10,42 @@ $(function () {
             var Email = $("input#email").val();
             var Subject = $("input#subject").val();
             var Message = $("textarea#message").val();
-
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
-            var script_url = "https://script.google.com/macros/s/AKfycbw6IQSwvjvcQgELrmUiEeyPoRznAkOR4j4VoSvqL8wh9kWbK_U2gu4BuDNX9Nh8yDU/exec";
-            var url = script_url + "?Name=" + Name + "&Email=" + Email + "&Subject=" + Subject +"&Message=" + Message + "&action=insert";
-
-            $.ajax({
-                crossDomain: true,
-                url: url,
-                method: "GET",
-                dataType: "jsonp",
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + Name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
+            var ip_url = "https://json.geoiplookup.io/";
+            var ip_address = '';
+            var address = '';
+            $.getJSON(ip_url, function (user_info) {
+                ip_address = user_info['ip'];
+                address = "postal_code - "+user_info['postal_code'] +", city - "+ user_info['city']+", country name - "+user_info['country_name'];
+                $this = $("#sendMessageButton");
+                $this.prop("disabled", true);
+                var script_url = "https://script.google.com/macros/s/AKfycbxsa4RuJk1b7rDSlxxA6gbSLLR-zdgjrfkRm0KqWq6Z5lA6znTkmpfRUiX8EYCvCPU/exec";
+                var url = script_url + "?Name=" + Name + "&Email=" + Email + "&Subject=" + Subject +"&Message=" + Message +"&IP=" + ip_address +"&address=" + address + "&action=insert";
+                $.ajax({
+                    crossDomain: true,
+                    url: url,
+                    method: "GET",
+                    dataType: "jsonp",
+                    cache: false,
+                    success: function () {
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                        $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                        $('#success > .alert-success').append('</div>');
+                        $('#contactForm').trigger("reset");
+                    },
+                    error: function () {
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                        $('#success > .alert-danger').append($("<strong>").text("Sorry " + Name + ", it seems that our mail server is not responding. Please try again later!"));
+                        $('#success > .alert-danger').append('</div>');
+                        $('#contactForm').trigger("reset");
+                    },
+                    complete: function () {
+                        setTimeout(function () {
+                            $this.prop("disabled", false);
+                        }, 1000);
+                    }
+                });
             });
         },
         filter: function () {
