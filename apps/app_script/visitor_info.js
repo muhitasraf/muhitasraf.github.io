11c4c4957f177e4b52bba4fb4b66eed9ebed48ec
muhitasraf <muhitasraf@gmail.com>
2023-07-27 10:12:24 +0600

    Prayer time code update

--- apps/app_script/visitor_info.js
+++ apps/app_script/visitor_info.js
@@ -7,6 +7,7 @@ function saveUserIpAddress(){
     var postal_code = '';
     var region = '';
     var district = '';
+    var lat_long = '';
     var isp = '';
     var flag = 0;
     $.ajax({
@@ -22,6 +23,7 @@ function saveUserIpAddress(){
             region = user_info['region'];
             district = user_info['district'];
             country_name = user_info['country_name'];
+            lat_long = user_info['latitude']+','+user_info['latitude'];
             var script_url = "https://script.google.com/macros/s/AKfycbwLO4W3Jqo9ivNZydyJdZyoKhDwmFDPDHHmVMuaDcUf4jkpV__zeGj88l4ruYktpjs/exec";
             var url_read = script_url + "?action=read";
             var user_data = "";
@@ -41,7 +43,7 @@ function saveUserIpAddress(){
                         }
                     }
                     if(flag == 1){
-                        var url = script_url + "?ip_address=" + ip_address + "&postal_code=" + postal_code + "&city=" + city +"&region=" + region +"&district=" + district +"&country_name=" + country_name + "&action=insert";
+                        var url = script_url + "?ip_address=" + ip_address + "&postal_code=" + postal_code + "&city=" + city +"&region=" + region +"&district=" + district +"&country_name=" + country_name + "&lat_long=" + lat_long + "&action=insert";
                         $.ajax({
                             crossDomain: true,
                             url: url,
